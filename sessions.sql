d7c0d400cfd43372fa54864f5355015e0244985e
mrurlwin <nz_danny_boy@hotmail.com>
2017-04-06 09:06:58 +1200

    Added database structure

---
+++ sessions.sql
@@ -0,0 +1,45 @@
+--
+-- Database: `sessions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(5, 'jason', 'jason@candle.com', '$2y$10$lNH3PVNR1l3liOf9MbhlOeqBgS/j2p4J.vN27YTuOmk6X3u4YI3WC');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
